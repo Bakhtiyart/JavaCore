e983f6d78d599c12615701d6921efac8d49ad739
Bakhtiyar <bakhtiyartayirov@gmail.com>
2021-03-25 17:08:14 +0500

    add files AlgorithmsDemo CompDemo TreeMapDemo2 TreeMapDemo2a

---
+++ src/main/java/com/bakhtiyart/javacore/chapter18/CompDemo2.java
@@ -0,0 +1,26 @@
+package com.bakhtiyart.javacore.chapter18;
+
+import java.util.*;
+
+public class CompDemo2 {
+    public static void main(String[] args) {
+
+        // передать компаратор с обратным упорядочением
+        // древовидному множеству типу TreeSet
+        TreeSet<String> ts = new TreeSet<>(
+                (aStr, bStr) -> bStr.compareTo(aStr));
+        // ввести элементы в древовидное множество
+        ts.add("C");
+        ts.add("A");
+        ts.add("B");
+        ts.add("E");
+        ts.add("F");
+        ts.add("D");
+
+        // вывести элементы из древовидного множества
+        for(String element : ts){
+            System.out.print(element + " ");
+            System.out.println();
+        }
+    }
+}
